ee9c78343000515c006f7c9b461fb344cf6f13ba
Anna Sores <annaa.sores@gmail.com>
2024-11-04 11:58:20 +0100

    move function added, game board examples changed

--- 1-D_tic-tac-toe.py
+++ 1-D_tic-tac-toe.py
@@ -1,4 +1,10 @@
 def evaluate(board):
+    """This function evaluates the game board. 
+    If x wins, returns 'x'
+    if O wins, returns 'o'
+    if it's draw, returns '!'
+    if the game is not finished, returns '-' """
+
     if "xxx" in board:
         return "x"
     elif "ooo" in board:
@@ -8,12 +14,29 @@ def evaluate(board):
     else:
         return "-"
 
-board1 = "---xxx---"
-board2 = "---ooo---"
-board3 = "oxxxoxoxo"
-board4 = "---xox---"
+def move(board, mark, position):
+    """This function is one move in tic-tac-toe, 
+    it takes the current game board, 
+    the mark (x or o) and 
+    the position (0-19), where the mark should be added.
+    And returns an updated game board."""
 
-print(evaluate(board1))
+    if board[position] == "-":
+        if position == 0:
+            new_board = mark + board[1:]
+        elif position == 19:
+            new_board = board[:19] + mark
+        elif position > 0 and position < 19:
+            new_board = board[:position] + mark + board[position +1 :]
+        return new_board
+    else:
+        print("position occupied")
+        return board
+        
+
+board1 = "--------------------"
+board2 = "oxox---------------o"
+
+print(move(board2, "x", 10))
+
 print(evaluate(board2))
-print(evaluate(board3))
-print(evaluate(board4))
