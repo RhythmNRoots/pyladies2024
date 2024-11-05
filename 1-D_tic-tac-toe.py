59cd1442a8f2b432ded10bf95e703ae3a9446701
Anna Sores <annaa.sores@gmail.com>
2024-11-05 12:27:34 +0100

    position function was added

--- 1-D_tic-tac-toe.py
+++ 1-D_tic-tac-toe.py
@@ -33,10 +33,31 @@ def move(board, mark, position):
         print("position occupied")
         return board
         
+def position():
+    """This function asks the player for the position desired
+    checks if it is a valid position and if it is a valid position 
+    it returns the desired position"""    
+    while True:
+        new_position = input("Which position do you want to mark? (0-19) ")
+        try:
+            new_position = int(new_position)
+            if new_position >= 0 and new_position <= 19:
+                return new_position
+            else:
+                print("Please enter an integer between 0-19.")
+        except:
+            print("Please enter an integer between 0-19.")
+
+def player_move(board):
+    """This function asks the player for the position,
+    returns the new game board or 
+    if the position is occupied asks for a new position."""
+    player_mark = 'x'
+    return move(board, player_mark, position())
 
 board1 = "--------------------"
 board2 = "oxox---------------o"
 
-print(move(board2, "x", 10))
+print(player_move(board2))
 
 print(evaluate(board2))
