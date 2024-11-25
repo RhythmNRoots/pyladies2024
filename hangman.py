f5637e424ec3c830a1489155a7998f5770120917
Anna Sores <annaa.sores@gmail.com>
2024-11-25 17:15:07 +0100

    Default status function was added to the file.

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 from random import randrange
 def picking_word_to_guess():
+    """This function randomly selects one of the 3 pre-defined words."""
     pick = randrange(3)
     if pick == 0:
         word_to_guess = 'journey'
@@ -9,4 +10,11 @@ def picking_word_to_guess():
         word_to_guess = 'friend'
     return word_to_guess
 
-print(picking_word_to_guess())
+def default_status(chosen_word):
+    """This function creates the default status for the game, a string which consists of as many "_" as the chosen word."""
+    print(chosen_word)
+    def_status = '_' *len(chosen_word)
+    return def_status
+
+word = picking_word_to_guess()
+print(default_status(word))
