48a57a51f98f79a43fec679a9373c551722765a3
Anna Sores <annaa.sores@gmail.com>
2024-11-26 16:28:27 +0100

    Functions for letter position and status update were created. Game is working now, changes are still needed.

--- hangman.py
+++ hangman.py
@@ -26,29 +26,58 @@ def picking_word_to_guess():
 
 def default_status(chosen_word):
     """This function creates the default status for the game, a string which consists of as many "_" as the chosen word."""
-    print(chosen_word)
     def_status = '_' *len(chosen_word)
     return def_status
 
-def hangman():
-    word_to_guess = picking_word_to_guess()
-    status = default_status(word_to_guess)
-    print(status)
-
-def evaluating(status, unsuccesful_tries):
-    if "_" not in status:
+def evaluating(current_status, unsuccessful_tries):
+    """This function evaluates if the game should end or not."""
+    if "_" not in current_status:
         print('Congratulations!')
         return 'end'
-    elif unsuccesful_tries >= 9:
+    elif unsuccessful_tries >= 9:
         print('Unfortunately you lost!')
         return 'end'
     else:
         return 'go'
 
-tries = 2
-word1 = "feiensg"
-print(evaluating(word1, tries))
+def letter_position(picked_letter, selected_string):
+    """This function returns the position of the desired letter in the word to guess"""
+    for i in range(len(selected_string)):
+        if selected_string[i] == picked_letter:
+            return i
+        else:
+            pass
 
+def update_status(letter, word_to_guess, status):
+    pos = letter_position(letter, word_to_guess)
+    updated_status = status[:pos] + letter + status[(pos + 1):] 
+    return(updated_status)
 
-word = picking_word_to_guess()
-print(default_status(word))
+def hangman_game():
+    word_to_guess = picking_word_to_guess()
+    starting_status = default_status(word_to_guess)
+    print(starting_status)
+    unsuccessful_tries = 0
+    status = starting_status
+    while evaluating(status, unsuccessful_tries) == 'go':
+        letter = input("Guess a letter: ")
+        if letter in word_to_guess:
+            status = update_status(letter, word_to_guess, status)
+            print(status)
+
+        else:
+            unsuccessful_tries += 1
+            print(f'Unsuccessful tries: ', unsuccessful_tries)
+            
+
+#tries = 2
+#word1 = "feiensg"
+#now_stat = "_______"
+#one_guess(word1, tries, now_stat)
+
+#print(evaluating(word1, tries))
+
+#word = picking_word_to_guess()
+#print(default_status(word))
+
+hangman_game()
